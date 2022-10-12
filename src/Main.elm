80fda1b149286570f23937e55d2f1b547b6f6ddf
micho <gatin13@yahoo.com>
2022-10-12 10:21:16 +0900

    Added initial main file

---
+++ src/Main.elm
@@ -0,0 +1,77 @@
+module Main exposing (main)
+
+import Browser exposing (Document)
+import Html exposing (Html, a, footer,h1, li, nav, text, ul)
+import Html.Attributes exposing (classList, href)
+import Html exposing (caption)
+
+
+type alias Model =
+  { page : Page }
+
+
+type Page
+    = Gallery
+    | Folders
+    | NotFound
+
+
+view : Model -> Document Msg
+view model =
+  let
+      content = text "This isn't even my final form!"
+  in
+  { title = "Photo Groove, SPA Style"
+  , body =
+    [ viewHeader model.page
+    , content
+    , viewFooter
+    ]
+  }
+
+
+viewFooter : Html msg
+viewFooter =
+  footer [] [ text "One is never alone with a rubber duck. -Douglas Adams" ]
+
+
+viewHeader : Page -> Html Msg
+viewHeader page =
+  let
+    logo = h1 [] [ text "Photo Groove" ]
+
+    links = ul []
+      [ navLink Folders { url = "/", caption = "Folders" }
+      , navLink Gallery { url = "/gallery", caption = "Gallery" }
+      ]
+
+    navLink : Page -> { url : String, caption : String } -> Html msg
+    navLink targetPage { url, caption } =
+      li [ classList [ ( "active", page == targetPage ) ] ]
+        [ a [ href url ] [ text caption ] ]
+  in
+  nav [] [ logo, links ]
+
+
+type Msg =
+  NothingYet
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
+update msg model =
+  ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program () Model Msg
+main =
+    Browser.document
+        { init = \_ -> ( { page = Folders }, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
